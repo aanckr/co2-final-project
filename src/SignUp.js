482c046c41e906b44fc329fc562fdce75c4f7043
Markus799 <116264057+Markus799@users.noreply.github.com>
2023-08-23 15:50:56 +0200

    added password to user table

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,39 +1,35 @@
 import React, { useState } from 'react';
 
 function SignUp(){
-    const [name, setName]= React.useState([]);
-    const [email, setEmail]= React.useState([]);
-    const [birthdate, setBirthdate]= React.useState([]);
-    const [sex, setSex]=  React.useState([]);
-    const [semester, setSemester]= React.useState([]);
-    const [course, setCourse]= React.useState([]);
-    const [userName, setUserName]= React.useState([]);
-    const [password, setPassword]= React.useState([]);
-    const [repeatPassword, setRepeatPassword]= React.useState([]);
+    const [name, setName]= React.useState('');
+    const [email, setEmail]= React.useState('');
+    const [birthdate, setBirthdate]= React.useState('');
+    const [sex, setSex]=  React.useState('');
+    const [semester, setSemester]= React.useState('');
+    const [course, setCourse]= React.useState('');
+    const [userName, setUserName]= React.useState('');
+    const [password, setPassword]= React.useState('');
+    const [repeatPassword, setRepeatPassword]= React.useState('');
     
     const handleSubmit = (event) => {
-<<<<<<< HEAD
-        if (name == null|| email == null || birthdate == null || password == null || repeatPassword == null){
-            console.log('A mendetory input (*) is missing.');
-            return;
-        } else if (password != repeatPassword) {
-            console.log('Passwords do not match.');
-            return;
-        };
-
-=======
->>>>>>> bbcb46f82b10ddb974ce048baa64369d7ff3c754
         event.preventDefault();
+        
+        if (!name || !userName || !email || !password || !repeatPassword) {
+            alert('Please fill out all required fields (*).');
+            return;
+        }
+
+        if (password !== repeatPassword) {
+            alert('Passwords do not match.');
+            return;
+        }
+        
         const requestOptions = {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({userName: userName, name: name, email: email, birthdate: birthdate, height: null, weight: null, sex: sex, semester: semester, course: course})
+            body: JSON.stringify({userName: userName, password: password, name: name, email: email, birthdate: birthdate, height: null, weight: null, sex: sex, semester: semester, course: course})
         };
-<<<<<<< HEAD
         
-=======
-
->>>>>>> bbcb46f82b10ddb974ce048baa64369d7ff3c754
         const dataFetch = async () => {
             const response = await (
                 await fetch(
@@ -64,7 +60,7 @@ function SignUp(){
             <input type='text' placeholder='Course of Study' name='course' maxLength={40} value={course} onChange={(e) => setCourse(e.target.value)}></input><br></br>
             <br></br>
             <input type='text' placeholder='* User Name' name='userName' maxLength={10} value={userName} onChange={(e) => setUserName(e.target.value)}></input><br></br>
-            <input type='password' placeholder='* Password' name='password' minLength={4} value={password} onChange={(e) => setPassword(e.target.value)}></input><br></br>
+            <input type='password' placeholder='* Password' name='password' minLength={4} maxLength={20} value={password} onChange={(e) => setPassword(e.target.value)}></input><br></br>
             <input type='password' placeholder='* Repeat Password' name='repeatPassword' minLength={4} value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)}></input><br></br>
             <br></br>
             <button type='submit'>Sign Up</button>
