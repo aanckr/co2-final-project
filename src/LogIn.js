8610bbb546b92d206cae8049a165d12c3880b0ed
Markus799 <116264057+Markus799@users.noreply.github.com>
2023-08-23 17:55:42 +0200

    added functionality and link to SignUp page

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,12 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function LogIn () {
-    const [userName, setUserName] = React.useState ([]);
-    const [password, setPassword] = React.useState ([]);
+    const [userName, setUserName] = React.useState ('');
+    const [password, setPassword] = React.useState ('');
+    const [logInEntries, setLogInEntries] = React.useState ([]);
 
-    const handleSubmit = (event) => (
+    const handleSubmit = (event) => {
         event.preventDefault()
-    );
+
+        var userExists = false;
+        var passwordCorrect = false;
+        var user;
+        for (let i = 0; i < logInEntries.length; i++) {
+            if (userName == logInEntries[i].user_name){
+                userExists = true;
+                user = userName;
+                if (password == logInEntries[i].password){
+                    passwordCorrect = true;
+                }
+            }
+        }
+
+        if (userExists == false || passwordCorrect == false) {
+            alert('Incorrect user data.')
+        }    
+    };
+    
+    useEffect(() => {
+        
+        const dataFetch = async () => {
+            const response = await fetch("http://localhost:3001/userLogIns")
+            const data = await response.json();
+            
+            setLogInEntries(data);
+        };
+        dataFetch();
+    }, []);
 
     return (
      <div>
@@ -16,6 +45,8 @@ function LogIn () {
             <input type='password' placeholder='Password' name='password' value={password} onChange={(e) => setPassword(e.target.value)}></input><br></br>
             <button type='submit'>Log In</button>
         </form>
+        <hr/>
+        <button onClick={() => window.location.href = 'http://localhost:3000/SignUp'}>Sign Up</button>
      </div>   
    );
 }
