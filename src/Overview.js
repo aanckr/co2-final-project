37572949a53c809559d0657d96581395d4847851
aanckr <a.annacker@student.xu-university.de>
2023-08-24 13:27:45 +0200

    Added new Components

--- src/Overview.js
+++ src/Overview.js
@@ -1,8 +1,15 @@
 import Header from './Header';
+import CurrentStatus from './CurrentStatus';
+import PersonalMessage from './PersonalMessage';
+import HealthData from './HealthData';
+
 function Overview() {
   return (
   <>
     <Header/>
+    <CurrentStatus/>
+    <PersonalMessage/>
+    <HealthData/>
   </>
   )
 }
