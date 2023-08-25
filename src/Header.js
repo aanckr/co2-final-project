1d4f5e6bbc5b2ab39e1840dbd63a52bef23eeb7d
aanckr <a.annacker@student.xu-university.de>
2023-08-25 16:07:22 +0200

    Added links to Overview.js and Profile.js

--- src/Header.js
+++ src/Header.js
@@ -1,32 +1,34 @@
 import "./Header.css";
-import profilePicture from './images/profile-pic.png';
+import profilePicture from "./images/profile-pic.png";
+import { link } from "react-router-dom";
 
 function Header() {
-    return (
-      <div id="header">
-        <nav id="navigation">
-          <ul>
-            <li>
-              <a href="#">Profile</a>
-            </li>
-            <li>
-              <a href="#">Track</a>
-            </li>
-            <li>
-              <a href="#">Analysis</a>
-            </li>
-            <li>
-              <a href="#">Blog</a>
-            </li>
-          </ul>
-        </nav>
-  
-        <div id="profile-picture">
-          <img src={profilePicture} alt="Profile Picture" />
-        </div>
+  return (
+    <div id="header">
+      <nav id="navigation">
+        <ul>
+          <li>
+            <a href={"./Overview"}>Profile</a>
+          </li>
+
+          <li>
+            <a href="#">Track</a>
+          </li>
+          <li>
+            <a href="#">Analysis</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+        </ul>
+      </nav>
+
+      <div id="profile-picture">
+      <a href={"./Profile"}><img src={profilePicture}  alt="Profile Picture" /></a>
+        
       </div>
-    );
-  }
-  
-  export default Header;
-  
+    </div>
+  );
+}
+
+export default Header;
