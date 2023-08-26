eaa705b7e465112f187d6e99be7a6ca350f92f4f
Markus799 <116264057+Markus799@users.noreply.github.com>
2023-08-26 10:17:50 +0200

    added Analysis Route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Overview from './Overview';
 import Profile from './Profile';
 import Privacy from './Privacy';
 import Blog from './Blog';
+import Analysis from './Analysis';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route path="/LogIn" element={<LogIn />} />
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/Overview" element={<Overview />} />
+          <Route path="/Analysis" element={<Analysis />} />
           <Route path="/Profile" element={<Profile />} />
           <Route path="/Privacy" element={<Privacy />} />
           <Route path="/Blog" element={<Blog />} />
