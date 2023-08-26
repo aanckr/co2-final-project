0da2aaccbf9617ffbedb19250de82ed6b81115a2
Markus799 <116264057+Markus799@users.noreply.github.com>
2023-08-26 11:34:22 +0200

    added user_name to href

--- src/Header.js
+++ src/Header.js
@@ -1,30 +1,32 @@
 import "./Header.css";
 import profilePicture from "./images/profile-pic.png";
-import { link } from "react-router-dom";
+import { link, useParams } from "react-router-dom";
 
 function Header() {
+  let { user_name } = useParams();
+  
   return (
     <div id="header">
       <nav id="navigation">
         <ul>
           <li>
-            <a href={"./Overview"}>Profile</a>
+            <a onClick={() => window.location.href = `http://localhost:3000/Overview/${user_name}`}>Profile</a>
           </li>
 
           <li>
             <a href="#">Track</a>
           </li>
           <li>
-            <a href={"./Analysis"}>Analysis</a>
+            <a onClick={() => window.location.href = `http://localhost:3000/Analysis/${user_name}`}>Analysis</a>
           </li>
           <li>
-            <a href={"./Blog"}>Blog</a>
+            <a onClick={() => window.location.href = `http://localhost:3000/Blog/${user_name}`}>Blog</a>
           </li>
         </ul>
       </nav>
 
       <div id="profile-picture">
-      <a href={"./Profile"}><img src={profilePicture}  alt="Profile Picture" /></a>
+      <a onClick={() => window.location.href = `http://localhost:3000/Profile/${user_name}`}><img src={profilePicture}  alt="Profile Picture" /></a>
         
       </div>
     </div>
