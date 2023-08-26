e020995a96788c91e7d9332c60c1a9c4a3c16f22
aanckr <a.annacker@student.xu-university.de>
2023-08-26 14:36:32 +0200

    Added Footer Component

--- src/Overview.js
+++ src/Overview.js
@@ -2,16 +2,19 @@ import CurrentStatus from './CurrentStatus';
 import Header from './Header';
 import HealthData from './HealthData';
 import PersonalMessage from './PersonalMessage';
+import Footer from './Footer'
+import "./Overview.css";
 
 function Overview() {
   return (
-  <>
+  <div className='color'>
     <Header/>
     <h1>Your Profile Overview</h1>
     <CurrentStatus/>
     <PersonalMessage/>
     <HealthData/>
-  </>
+    <Footer/>
+  </div>
   )
 }
 
