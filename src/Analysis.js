61c5ec1fdec411775b1e1b064c26c96cce91dd90
Markus799 <116264057+Markus799@users.noreply.github.com>
2023-08-26 11:19:59 +0200

    added page structure

--- src/Analysis.js
+++ src/Analysis.js
@@ -4,6 +4,23 @@ function Analysis(){
     return (
         <div>
             <Header />
+            <h3>General Feeling</h3>
+
+            <hr/>
+
+            <h3>Mental Helth Information</h3>
+        
+            <hr/>
+
+            <h3>Study and Workload Information </h3>
+
+            <hr/>
+
+            <h3>Lifestyle Information</h3>
+
+            <hr/>
+
+            <h3>Intersting Blogs</h3>
         </div>
     )
 };
