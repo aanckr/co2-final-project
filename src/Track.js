aa4c5af72ec3afff7a780bd002ce1745fa71f5ad
Markus799 <116264057+Markus799@users.noreply.github.com>
2023-08-28 22:19:28 +0200

    added information if daily form is done

--- src/Track.js
+++ src/Track.js
@@ -4,10 +4,15 @@ import StudyAndWorkload from './StudyAndWorkload';
 import Lifestyle from './Lifestyle';
 import Footer from './Footer'
 import "./Track.css";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 
 function Track(){
+    let { user_name } = useParams();
+
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
     const [generalMood, setGeneralMood] = useState();
     const [stressLevel, setStressLevel] = useState();
     const [sleepQuality, setSleepQualtiy] = useState();
@@ -24,23 +29,100 @@ function Track(){
     const [drugUse, setDrugUse] = useState();
 
     var today = new Date();
- 
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
- 
     var now = today.toLocaleString('en-US', options);
+    var date = today.toISOString().slice(0, 10);
+    console.log(date)
+    useEffect(() => {
+        
+        const dataFetch = async () => {
+            const response = await fetch(`http://localhost:3001/mentalHealth/${user_name}/${date}`)
+            const data = await response.json();
+            console.log(data[0])
+            if (data[0]){
+                setFormSubmitted(true);
+            };
+            
+        };
+        dataFetch();
+    }, [user_name]);
+
+   
+
+    const handleSubmit = (event) => {
+
+        const finalGeneralMood = generalMood ? generalMood : null;
+        const finalStressLevel = stressLevel ? stressLevel : null;
+        const finalSleepQuality = sleepQuality ? sleepQuality : null;
+        const finalSleepDuration = sleepDuration ? sleepDuration : null;
+
+        const finalStudyTime = studyTime ? studyTime : null;
+        const finalLearningTime = learningTime ? learningTime : null;
+        const finalWorkTime = workTime ? workTime : null;
+
+        const finalDietaryHabits = dietaryHabits ? dietaryHabits : null;
+        const finalActivityTime = activityTime ? activityTime : null;
+        const finalTobaccoUse = tobaccoUse ? tobaccoUse : null;
+        const finalAlcoholConsumption = alcoholConsumption ? alcoholConsumption : null;
+        const finalDrugUse = drugUse ? drugUse : null;
+        
+        const mentalHealthOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({user_name: user_name, date: date, general_mood: finalGeneralMood, stress_level: finalStressLevel, sleep_quality: finalSleepQuality, sleep_duration: finalSleepDuration})
+        };
+
+        const studyAndWorkloadOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({user_name: user_name, date: date, study_time: finalStudyTime, learning_time: finalLearningTime, work_time: finalWorkTime})
+        };
+
+        const lifestyleOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({user_name: user_name, date: date, dietary_habits: finalDietaryHabits, activity_time: finalActivityTime, tobacco_use: finalTobaccoUse, alcohol_consumption: finalAlcoholConsumption, drug_use: finalDrugUse})
+        };
+        
+        const dataFetch = async () => {
+            const mentalHealthResponse = await fetch("http://localhost:3001/mentalHealth", mentalHealthOptions);
+            const mentalHealthData = await mentalHealthResponse.json();
+
+            const studyAndWorkloadResponse = await fetch("http://localhost:3001/studyAndWorkload", studyAndWorkloadOptions);
+            const studyAndWorkloadData = await studyAndWorkloadResponse.json();
+
+            const lifestyleResponse = await fetch("http://localhost:3001/lifestyle", lifestyleOptions);
+            const lifestyleData = await lifestyleResponse.json();
+
+            console.log({mentalHealthData, studyAndWorkloadData, lifestyleData});
+        }
+        dataFetch();
+        window.location.href = `http://localhost:3000/Track/${user_name}`;
+    };
 
     return(
         <div className='color'>
+            
             <Header/>
-            <span>
-                <h3 id='date'>{now}</h3>
-            </span>
-            <MentalHealth setGeneralMood={setGeneralMood} setStressLevel={setStressLevel} setSleepQualtiy={setSleepQualtiy} setSleepDuration={setSleepDuration} />
-            <StudyAndWorkload setStudyTime={setStudyTime} setLearningTime={setLearningTime} setWorkTime={setWorkTime} />
-            <Lifestyle setDietaryHabits={setDietaryHabits} setActivityTime={setActivityTime} setTobaccoUse={setTobaccoUse} setAlcoholConsumption={setAlcoholConsumption} setDrugUse={setDrugUse} />
-            <button className='save-button'>Save</button> 
-            <Footer/>
-
+            {!formSubmitted ? (
+                <div>
+                    <span>
+                        <h3 id='date'>{now}</h3>
+                    </span>
+                    <MentalHealth setGeneralMood={setGeneralMood} setStressLevel={setStressLevel} setSleepQualtiy={setSleepQualtiy} setSleepDuration={setSleepDuration} />
+                    <StudyAndWorkload setStudyTime={setStudyTime} setLearningTime={setLearningTime} setWorkTime={setWorkTime} />
+                    <Lifestyle setDietaryHabits={setDietaryHabits} setActivityTime={setActivityTime} setTobaccoUse={setTobaccoUse} setAlcoholConsumption={setAlcoholConsumption} setDrugUse={setDrugUse} />
+                    <button onClick={handleSubmit} type='submit' className='save-button'>Save</button>
+                </div> 
+            ) : (
+                <div>
+                    <span>
+                            <h3 id='date'>{now}</h3>
+                    </span>
+                    <h1>Daily Form Done!</h1>
+                </div>
+            )}
+            <Footer/>   
         </div>
     )
 }
