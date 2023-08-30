14a20dad095485ec0f81fddaac58d93a0331a4fd
Anna <116264008+Annascho@users.noreply.github.com>
2023-08-30 14:59:16 +0200

    Update HeaderStart
    Styling of header start improved and icons added

--- src/HeaderStart.js
+++ src/HeaderStart.js
@@ -1,4 +1,7 @@
 import "./HeaderStart.css";
+import SignUp from './images/SignUp.png';
+import LogIn from './images/LogIn.png';
+import Pluto from './images/pluto.png';
 const React = require('react');
 const { Link } = require('react-router-dom');
 
@@ -6,12 +9,13 @@ function HeaderStart() {
   return (
     <div className="header_start">
       <nav className="navigation_start">
+        <div><img className="logo" src={Pluto} alt="Pluto-Icon"/></div>
         <ul>
-          <li><h3 className="logo">Mental Health Tracker</h3></li>
-          <li><Link to="/LogIn">LogIn</Link></li>
-          <li><Link to="/SignUp">SignUp</Link></li>
+          <li><Link to="/LogIn"><img className="start-icon" src={LogIn} alt="LogIn-Icon" />LogIn</Link></li>
+          <li><Link to="/SignUp"><img className="start-icon" src={SignUp} alt="SignUp-Icon" />SignUp</Link></li>
         </ul>
       </nav>
+      <h3 className="app_name">Mental Health Tracker</h3>
     </div>
   );
 }
