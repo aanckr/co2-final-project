d8e3c88296f902778f49f0a236a11ff586730bc0
Anna <116264008+Annascho@users.noreply.github.com>
2023-08-30 13:25:24 +0200

    Update Style Footer
    Styling was improved

--- src/FooterStart.js
+++ src/FooterStart.js
@@ -1,8 +1,8 @@
-import "./Footer.css"
+import "./FooterStart.css"
 const React = require('react');
 const { Link } = require('react-router-dom');
 
-function Footer_start() {
+function FooterStart() {
     return (
         <footer>
             <div className="waves">
@@ -10,26 +10,25 @@ function Footer_start() {
                 <div className="wave" id="wave2"></div>
                 <div className="wave" id="wave3"></div>
             </div>
-            <div className="footer-col-1">
+            <div className="footer-start-1">
                 <h3>JOIN US</h3>
                 <Link to="/logIn">LogIn</Link>
                 <Link to="/signUp">SignUp</Link>
             </div>
-            <div className="footer-col-2">
+            <div className="footer-start-2">
                 <h3>NEWSLETTER</h3>
-                <form >
-                    <input id="email" type="email" placeholder="Your Email Address" required></input>
-                    <br />
+                <form className="email">
+                    <input type="email" placeholder="Your Email Address" required></input>
                     <button className='button-news' type="submit">SUBSCRIBE NOW</button>
                 </form>
             </div>
-            <div className="footer-col-3">
+            <div className="footer-start-3">
                 <h3>CONTACT</h3>
                 <p>XU University Students</p>
                 <p>August-Bebel-Str. 26-53</p>
                 <p>14482 Potsdam</p>
             </div>
-            <div className='footer-col-1'>
+            <div className='footer-start-4'>
                 <h3>LEGAL</h3>
                 <Link to="/privacy">Privacy</Link>
             </div>
@@ -37,5 +36,5 @@ function Footer_start() {
     );
 }
 
-export default Footer_start;
+export default FooterStart;
 
